b91be9767fdb92010afabce50a86a12b3de48948
Heman <gharibnejad@cpi.com>
2022-08-19 09:08:02 -0400

    merged the changes committed by Ryan on 8/18

--- Source/two_level_atom.f90
+++ Source/two_level_atom.f90
@@ -7,7 +7,7 @@ program two_level_atom
   implicit none
 
     complex(8), dimension(2,2) :: H, A
-    complex(8), dimension(2)   :: tl_psi, tl_integral, cn_solution, b, analytic_soln, soln_error
+    complex(8), dimension(2)   :: tl_psi, tl_integral, cn_solution, b, analytic_soln, soln_error, dummy
     complex(8), allocatable    :: tl_inhomogeneity(:,:), tl_iterate(:,:), tl_phi(:,:), model_soln(:,:), model_diff(:,:)
     real(8), allocatable       :: nc_points(:)
     real(8), allocatable       :: nc_weights(:,:), weights(:,:)
@@ -49,118 +49,124 @@ program two_level_atom
        H(1,2) = tl_pulse_midpoint
        H(2,1) = tl_pulse_midpoint
 
-       ! compute quadrature points and weights
-       call lgr_weights(time, time+dt, nc_points, nc_weights, n-2, quad_type)
-
-       ! compute composite weights
-       weights(:,1) = nc_weights(:,1)
-       do q=2,n-1
-           do s = 1,n
-              weights(s,q) = weights(s,q-1) + nc_weights(s,q)
-           end do
-       end do
-
-       ! compute inhomogeneity at the quadrature points
-       tl_inhomogeneity(:,1) = tl_psi(:)
-       do i=2,n
-          theta = tl_pulse_midpoint*(nc_points(i) - nc_points(1))
-          tl_inhomogeneity(1,i) = cos(theta)*tl_psi(1) - ii*sin(theta)*tl_psi(2)
-          tl_inhomogeneity(2,i) = -ii*sin(theta)*tl_psi(1) + cos(theta)*tl_psi(2)
-       end do
-
-       ! iterate starting with the inhomogeneity
-       tl_converged = .FALSE.
-       tl_iterate(:,1:n) = tl_inhomogeneity(:,1:n)
-       
-
-       ! Short-time approximation
+       ! short-time approximation
        if (it_type == 'short_time') then
-          tl_psi(:) = tl_inhomogeneity(:,n)
+          theta = tl_pulse_midpoint*dt
+          dummy(1) = cos(theta)*tl_psi(1) - ii*sin(theta)*tl_psi(2)
+          dummy(2) = -ii*sin(theta)*tl_psi(1) + cos(theta)*tl_psi(2)
           
+          tl_psi(:) = dummy(:)
 
-       ! Jacobi iteration
-       else if (it_type == 'jacobi') then
-          do while(.not. tl_converged)
-             tl_phi(:,1:n) = tl_iterate(:,1:n)
+       else
+          ! compute quadrature points and weights
+          call lgr_weights(time, time+dt, nc_points, nc_weights, n-2, quad_type)
 
-             do u=2,n
-                b(:) = tl_inhomogeneity(:,u)
-                
-                do p=1,n
-                   theta = tl_pulse_midpoint*(nc_points(u) - nc_points(p))
-                   E_t = 0.5d0*E_0*sin(pi*nc_points(p)/t_intv)*sin(pi*nc_points(p)/t_intv) - tl_pulse_midpoint
-                   b(1) = b(1) - ii*weights(p,u-1)*(cos(theta)*E_t*tl_phi(2,p) - ii*sin(theta)*E_t*tl_phi(1,p))
-                   b(2) = b(2) - ii*weights(p,u-1)*(cos(theta)*E_t*tl_phi(1,p) - ii*sin(theta)*E_t*tl_phi(2,p))
-                end do
-
-                tl_iterate(:,u) = b(:)
+          ! compute composite weights
+          weights(:,1) = nc_weights(:,1)
+          do q=2,n-1
+             do s = 1,n
+                weights(s,q) = weights(s,q-1) + nc_weights(s,q)
              end do
-
-             tl_converged = convergence_test(tl_phi, tl_iterate, it_tolerance)
-             count = count + 1
-
-             if (count >= it_cap) then
-                tl_converged = .TRUE.
-             end if
-          
           end do
 
-          tl_psi(:) = tl_iterate(:,n)
+          ! compute inhomogeneity at the quadrature points
+          tl_inhomogeneity(:,1) = tl_psi(:)
+          do i=2,n
+             theta = tl_pulse_midpoint*(nc_points(i) - nc_points(1))
+             tl_inhomogeneity(1,i) = cos(theta)*tl_psi(1) - ii*sin(theta)*tl_psi(2)
+             tl_inhomogeneity(2,i) = -ii*sin(theta)*tl_psi(1) + cos(theta)*tl_psi(2)
+          end do
+
+          ! iterate starting with the inhomogeneity
+          tl_converged = .FALSE.
+          tl_iterate(:,1:n) = tl_inhomogeneity(:,1:n)
+
+          ! Jacobi iteration
+          if (it_type == 'jacobi') then
+             do while(.not. tl_converged)
+                tl_phi(:,1:n) = tl_iterate(:,1:n)
+
+                do u=2,n
+                   b(:) = tl_inhomogeneity(:,u)
+                
+                   do p=1,n
+                      theta = tl_pulse_midpoint*(nc_points(u) - nc_points(p))
+                      E_t = 0.5d0*E_0*sin(pi*nc_points(p)/t_intv)*sin(pi*nc_points(p)/t_intv) - tl_pulse_midpoint
+                      b(1) = b(1) - ii*weights(p,u-1)*(cos(theta)*E_t*tl_phi(2,p) - ii*sin(theta)*E_t*tl_phi(1,p))
+                      b(2) = b(2) - ii*weights(p,u-1)*(cos(theta)*E_t*tl_phi(1,p) - ii*sin(theta)*E_t*tl_phi(2,p))
+                   end do
+
+                   tl_iterate(:,u) = b(:)
+                end do
+
+                tl_converged = convergence_test(tl_phi, tl_iterate, it_tolerance)
+                count = count + 1
+
+                if (count >= it_cap) then
+                   tl_converged = .TRUE.
+                end if
+          
+             end do
+
+             tl_psi(:) = tl_iterate(:,n)
           
 
-       ! Gauss-Seidel iteration
-       else if (it_type == 'gauss_seidel') then
-          do while(.not. tl_converged)
-             tl_phi(:,1:n) = tl_iterate(:,1:n)
+          ! Gauss-Seidel iteration
+          else if (it_type == 'gauss_seidel') then
+             do while(.not. tl_converged)
+                tl_phi(:,1:n) = tl_iterate(:,1:n)
          
-             do j=2,n
-                A(1,1) = 1
-                A(1,2) = ii*weights(j,j-1)*(0.5d0*E_0*sin(pi*nc_points(j)/t_intv)*sin(pi*nc_points(j)/t_intv) - tl_pulse_midpoint)
-                A(2,1) = A(1,2)
-                A(2,2) = A(1,1)
+                do j=2,n
+                   A(1,1) = 1
+                   A(1,2) = ii*weights(j,j-1)*(0.5d0*E_0*sin(pi*nc_points(j)/t_intv)*sin(pi*nc_points(j)/t_intv)&
+                        - tl_pulse_midpoint)
+                   A(2,1) = A(1,2)
+                   A(2,2) = A(1,1)
              
-                b(:) = tl_inhomogeneity(:,j)
+                   b(:) = tl_inhomogeneity(:,j)
              
-                do k=1,j-1
-                  theta = tl_pulse_midpoint*(nc_points(j) - nc_points(k))
-                  E_t = 0.5d0*E_0*sin(pi*nc_points(k)/t_intv)*sin(pi*nc_points(k)/t_intv) - tl_pulse_midpoint
-                  b(1) = b(1) - ii*weights(k,j-1)*(cos(theta)*E_t*tl_iterate(2,k) - ii*sin(theta)*E_t*tl_iterate(1,k))
-                  b(2) = b(2) - ii*weights(k,j-1)*(cos(theta)*E_t*tl_iterate(1,k) - ii*sin(theta)*E_t*tl_iterate(2,k))
-                end do
+                   do k=1,j-1
+                      theta = tl_pulse_midpoint*(nc_points(j) - nc_points(k))
+                      E_t = 0.5d0*E_0*sin(pi*nc_points(k)/t_intv)*sin(pi*nc_points(k)/t_intv) - tl_pulse_midpoint
+                      b(1) = b(1) - ii*weights(k,j-1)*(cos(theta)*E_t*tl_iterate(2,k) - ii*sin(theta)*E_t*tl_iterate(1,k))
+                      b(2) = b(2) - ii*weights(k,j-1)*(cos(theta)*E_t*tl_iterate(1,k) - ii*sin(theta)*E_t*tl_iterate(2,k))
+                   end do
 
-                do l=j+1,n
-                   theta = tl_pulse_midpoint*(nc_points(j) - nc_points(l))
-                   E_t = 0.5d0*E_0*sin(pi*nc_points(l)/t_intv)*sin(pi*nc_points(l)/t_intv) - tl_pulse_midpoint
-                   b(1) = b(1) - ii*weights(l,j-1)*(cos(theta)*E_t*tl_phi(2,l) - ii*sin(theta)*E_t*tl_phi(1,l))
-                   b(2) = b(2) - ii*weights(l,j-1)*(cos(theta)*E_t*tl_phi(1,l) - ii*sin(theta)*E_t*tl_phi(2,l))
-                end do
+                   do l=j+1,n
+                      theta = tl_pulse_midpoint*(nc_points(j) - nc_points(l))
+                      E_t = 0.5d0*E_0*sin(pi*nc_points(l)/t_intv)*sin(pi*nc_points(l)/t_intv) - tl_pulse_midpoint
+                      b(1) = b(1) - ii*weights(l,j-1)*(cos(theta)*E_t*tl_phi(2,l) - ii*sin(theta)*E_t*tl_phi(1,l))
+                      b(2) = b(2) - ii*weights(l,j-1)*(cos(theta)*E_t*tl_phi(1,l) - ii*sin(theta)*E_t*tl_phi(2,l))
+                   end do
 
-                call zgesv(2,1,A,2,IPIV,b,2,info)
-                tl_iterate(:,j) = b(:)
+                   call zgesv(2,1,A,2,IPIV,b,2,info)
+                   tl_iterate(:,j) = b(:)
                 
-             end do
+                end do
 
-             tl_converged = convergence_test(tl_phi, tl_iterate, it_tolerance)
-             count = count + 1
+                tl_converged = convergence_test(tl_phi, tl_iterate, it_tolerance)
+                count = count + 1
 
-             if (count >= it_cap) then
-                tl_converged = .TRUE.
-             end if
+                if (count >= it_cap) then
+                   tl_converged = .TRUE.
+                end if
             
-          end do
+             end do
    
-          tl_psi(:) = tl_iterate(:,n)
+             tl_psi(:) = tl_iterate(:,n)
+
+          end if
 
        end if
-       
-       ! compare converged result with analytic solution
-       do r = 1,n
-          analytic_soln(1) = cos(0.25d0*E_0*(nc_points(r) - (t_intv/(2*pi))*sin(2*pi*nc_points(r)/t_intv)))
-          analytic_soln(2) = -ii*sin(0.25d0*E_0*(nc_points(r) - (t_intv/(2*pi))*sin(2*pi*nc_points(r)/t_intv)))
 
-          solution_error_g = abs(abs(analytic_soln(1))*abs(analytic_soln(1)) - abs(tl_iterate(1,r))*abs(tl_iterate(1,r)))
-          solution_error_e = abs(abs(analytic_soln(2))*abs(analytic_soln(2)) - abs(tl_iterate(2,r))*abs(tl_iterate(2,r)))
-     
+       ! compare converged result with analytic solution
+       if (it_type == 'short_time') then
+          analytic_soln(1) = cos(0.25d0*E_0*((time+dt) - (t_intv/(2*pi))*sin(2*pi*(time+dt)/t_intv)))
+          analytic_soln(2) = -ii*sin(0.25d0*E_0*((time+dt) - (t_intv/(2*pi))*sin(2*pi*(time+dt)/t_intv)))
+
+          solution_error_g = abs(abs(analytic_soln(1))*abs(analytic_soln(1)) - abs(tl_psi(1))*abs(tl_psi(1)))
+          solution_error_e = abs(abs(analytic_soln(2))*abs(analytic_soln(2)) - abs(tl_psi(2))*abs(tl_psi(2)))
+
           if (max_soln_error_g < solution_error_g) then
              max_soln_error_g = solution_error_g
           end if
@@ -169,9 +175,26 @@ program two_level_atom
              max_soln_error_e = solution_error_e
           end if
 
-       end do
+       else
+          do r = 1,n
+             analytic_soln(1) = cos(0.25d0*E_0*(nc_points(r) - (t_intv/(2*pi))*sin(2*pi*nc_points(r)/t_intv)))
+             analytic_soln(2) = -ii*sin(0.25d0*E_0*(nc_points(r) - (t_intv/(2*pi))*sin(2*pi*nc_points(r)/t_intv)))
 
-       write(73,*) nc_points(n), solution_error_g, solution_error_e
+             solution_error_g = abs(abs(analytic_soln(1))*abs(analytic_soln(1)) - abs(tl_iterate(1,r))*abs(tl_iterate(1,r)))
+             solution_error_e = abs(abs(analytic_soln(2))*abs(analytic_soln(2)) - abs(tl_iterate(2,r))*abs(tl_iterate(2,r)))
+     
+             if (max_soln_error_g < solution_error_g) then
+                max_soln_error_g = solution_error_g
+             end if
+
+             if (max_soln_error_e < solution_error_e) then
+                max_soln_error_e = solution_error_e
+             end if
+          end do
+          
+       end if
+
+       write(73,*) time+dt, solution_error_g, solution_error_e
 
        if (max_count < count) then
           max_count = count
